e1dfe38e665c782fef6f447919aa9161f347cb2c
ALysan <angelina_lysan@mail.ru>
2014-10-26 12:41:41 +0200

    speciality

--- speciality.php
+++ speciality.php
@@ -2,39 +2,28 @@
  <?php include('block/left-menu.php');?>
    
 	<!-- Контент -->
+<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+  <div class="row">
 
-<!--
+<p><h1 style="color:SeaGreen"><big><b><i><center>СПЕЦИАЛЬНОСТИ КАФЕДРЫ</center></i></b></big></h1></p>
+<br></br> 
+<p><img src="flowers.jpg" align="right">
+<p><img src="flowers.jpg" align="left">
 <center>
-<p>Специальности</p>
-<br> </br>
-<p><img src="image/comp.jpg" align="center"></p>
-<li><a href="system.php"><h1>Системное проектирование </h1></a></li>
-<br> </br>
-<li><a href="inform.php"><h1>Информатика</h1></a></li>
-<br> </br>
-<li><a href="mathematic.php"><h1>Прикладная математика</h1></a></li>
+<br></br>
+<br></br>
+<a href="system.php"><h1 style="color:Chocolate">Системное проектирование </h1></a>
+<br></br>
+<a href="inform.php"><h1 style="color:Chocolate">Информатика</h1></a>
+<br></br>
+<a href="mathematic.php"><h1 style="color:Chocolate">Прикладная математика</h1></a></p>
 
-</center>
--->
 
-<div class="container">
-    <div class="row">
-      	<div class="col-md-12">
-      		<div class="col-md-11 col-md-offset-1">
-      			<h1 align="center" class="blog-post-title">Специальности</h1>
-	      		<p style="text-align:center; margin-top: 60px; margin-bottom: 60px;"><img src="image/comp.jpg"></p>
-				
-				<div style="text-align: center; margin-bottom: 60px">
-					<li><a href="system.php"><h1>Системное проектирование </h1></a></li>
-					<br> </br>
-					<li><a href="inform.php"><h1>Информатика</h1></a></li>
-					<br> </br>
-					<li><a href="mathematic.php"><h1>Прикладная математика</h1></a></li>
-				</div >
-      		</div>
-		</div>	
-  	</div>
-</div>
+ </center>
+
+ 
+<br> </br><br> </br><br> </br><br> </br><br>
+
 
 	<!-- Подвал -->
  <?php include('block/footer.php');?>
