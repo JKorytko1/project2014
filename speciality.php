4d51cf043f6d40cf93dbadbfed93b2c756afc5aa
ALysan <angelina_lysan@mail.ru>
2014-10-28 22:46:15 +0200

    speciality

--- speciality.php
+++ speciality.php
@@ -5,25 +5,22 @@
 <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
   <div class="row">
 
-<p><h1 style="color:SeaGreen"><big><b><i><center>СПЕЦИАЛЬНОСТИ КАФЕДРЫ</center></i></b></big></h1></p>
-<br></br> 
-<p><img src="flowers.jpg" align="right">
-<p><img src="flowers.jpg" align="left">
-<center>
-<br></br>
-<br></br>
-<a href="system.php"><h1 style="color:Chocolate">Системное проектирование </h1></a>
-<br></br>
-<a href="inform.php"><h1 style="color:Chocolate">Информатика</h1></a>
-<br></br>
-<a href="mathematic.php"><h1 style="color:Chocolate">Прикладная математика</h1></a></p>
+  <img src="image/comp.jpg" width="1100" height="800">
+  </br><br>   </br><br> 
+<div style="width:1000px; height:700px; margin-top:-800px; text-align:center; color:#FFE4E1">
+<h1><big><b><i><center>СПЕЦИАЛЬНОСТИ КАФЕДРЫ</center></i></b></big></h1>
+<br></br></br><br>   </br><br> 
 
+ <a href="inform.php"><h1 style="color:White"><big><b><i><center>Информатика</center></i></b></big></h1></a>
+<br></br>
 
- </center>
+ <a href="mathematic.php"><h1 style="color:White"><big><b><i><center>Прикладная математика</center></i></b></big></h1></a>
+<br></br>
+ <a href="system.php"> <h1 style="color:White"> <big><b><i><center>Системное проектирование</center></i></b></big></h1></a>
 
- 
-<br> </br><br> </br><br> </br><br> </br><br>
+</div>
 
+<br> </br>
 
 	<!-- Подвал -->
  <?php include('block/footer.php');?>
