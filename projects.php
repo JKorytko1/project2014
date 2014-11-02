bf8075bd8bb388dee139e6a83c1589a0c94984f3
PSenko <pavelsenko92@gmail.com>
2014-11-02 11:59:37 +0200

    projects

---
+++ projects.php
@@ -0,0 +1,67 @@
+﻿ <?php include('block/header.php');?>
+ <?php include('block/left-menu.php');?>
+
+ 
+    <!-- Контент -->
+	<div class="container">
+    <div class="row">
+      	<div class="col-md-10 col-md-offset-2">
+      		<div class="blog-post" style="margin-top: 35px;">
+	  
+	  <div style="border: 2px solid #9f955c; background: #96F7F4; padding: 13px; border-top-left-radius: 15px; border-top-right-radius: 15px; border-bottom-right-radius: 15px; border-bottom-left-radius: 15px; text-align: justify;">
+       <ins><h1>Ветроэнергетическая установка (ВЭУ)</h1></ins>
+        <p>
+        Проект ВЭУ проводился на кафедре СПУ под руководством к.т.н. Корытко Ю.Н.
+        </p>
+        <p><a href="p_1.php" class="btn btn-primary btn-lg" role="button">Узнать больше »</a></p>
+		</div><br></br>
+		
+		<div style="border: 2px solid #9f955c; background: #96F7F4; padding: 13px; border-top-left-radius: 15px; border-top-right-radius: 15px; border-bottom-right-radius: 15px; border-bottom-left-radius: 15px; text-align: justify;">
+		<ins><h1>Разработка конструкции и системы управления мини-дирижаблем</h1></ins>
+        <p>
+        Разработка конструкции и системы управления мини-дирижаблем. Проект студентов 4 и 5 курсов специальностей "Информатика" и "Системное проектирование". Выполняется под руководством И.В.Наумова
+        </p>
+        <p><a href="p_2.php" class="btn btn-primary btn-lg" role="button">Узнать больше »</a></p>
+		</div><br></br>
+		
+		
+		<div style="border: 2px solid #9f955c; background: #96F7F4; padding: 13px; border-top-left-radius: 15px; border-top-right-radius: 15px; border-bottom-right-radius: 15px; border-bottom-left-radius: 15px; text-align: justify;">
+		<ins><h1>Устройство для бесконтактного управления</h1></ins>
+        <p>
+        В современных цифровых устройствах все больше используется интуитивный ввод информации. Это сенсорные экраны и датчики, реагирующие на движения оператора. Они обеспечивают простоту обучения и легкость освоения новых интерфейсов.
+        </p>
+        <p><a href="p_3.php" class="btn btn-primary btn-lg" role="button">Узнать больше »</a></p>
+		</div><br></br>
+		
+		<div style="border: 2px solid #9f955c; background: #96F7F4; padding: 13px; border-top-left-radius: 15px; border-top-right-radius: 15px; border-bottom-right-radius: 15px; border-bottom-left-radius: 15px; text-align: justify;">
+		<ins><h1>Семантический анализ текста и автоматическая генерация медиаконтента</h1></ins>
+        <p>
+       Разработанный плагин производит разбор текста статьи или записи вблоге. После разбора строится частотная таблица слов, используемых в тексте и производится поиск изображений, соответствующих этим словам или их синонимам.
+        </p>
+        <p><a href="p_4.php" class="btn btn-primary btn-lg" role="button">Узнать больше »</a></p>
+		</div><br></br>
+		
+		<div style="border: 2px solid #9f955c; background: #96F7F4; padding: 13px; border-top-left-radius: 15px; border-top-right-radius: 15px; border-bottom-right-radius: 15px; border-bottom-left-radius: 15px; text-align: justify;">
+		<ins><h1>Разработка программы для анализа и прогноза скорости и направления ветра</h1></ins>
+        <p>
+        Создана программа оценки воздушных потоков в данной местности на основании данных синоптических сайтов.
+        </p>
+        <p><a href="p_5.php" class="btn btn-primary btn-lg" role="button">Узнать больше »</a></p>
+		</div><br></br>
+		
+		<div style="border: 2px solid #9f955c; background: #96F7F4; padding: 13px; border-top-left-radius: 15px; border-top-right-radius: 15px; border-bottom-right-radius: 15px; border-bottom-left-radius: 15px; text-align: justify;">
+		<ins><h1>Навигационная система с трансляцией телеметрии</h1></ins>
+        <p>
+        Разработана бортовая система, обеспечивающая управление летательным аппаратом и трансляцию телеметрии. Телеметрическая информация дублируется на стороннем носителе.
+        </p>
+        <p><a href="p_6.php" class="btn btn-primary btn-lg" role="button">Узнать больше »</a></p>
+		</div><br></br>
+		
+		   </div>
+          </div>
+      </div>
+    </div>
+	
+ 
+<!-- footer -->
+ <?php include('block/footer.php');?>
