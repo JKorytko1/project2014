accead27234f731ce2f9a1d3bdf7297afa687ca9
ALysan <angelina_lysan@mail.ru>
2014-11-09 12:09:22 +0200

    speciality

--- speciality.php
+++ speciality.php
@@ -26,22 +26,22 @@
 		<div class="row">
 			<div class="col-md-10 col-md-offset-2">
 				<center>
-					<!-- <img src="image/comp.jpg" width="1100" height="800"> -->
-					<img src="image/comp.jpg" width="1000" height="800">
+					<!-- <img src="assets/images/comp.jpg" width="1100" height="800"> -->
+					<img src="assets/images/comp.jpg" width="1000" height="800">
 				</center>
 
 				</br><br>   </br><br> 
 				
 				<div style="width:1000px; height:700px; margin-top:-800px; text-align:center; color:#FFE4E1">
-					<h1><big><b><i><center>СПЕЦИАЛЬНОСТИ КАФЕДРЫ</center></i></b></big></h1>
+					<h1><big><b><center>СПЕЦИАЛЬНОСТИ КАФЕДРЫ</center></b></big></h1>
 					<br></br></br><br>   </br><br> 
 
-					 <a href="inform.php"><h1 style="color:White"><big><b><i><center>Информатика</center></i></b></big></h1></a>
+					 <a href="inform.php"><h1 style="color:White"><big><center>Информатика</center></big></h1></a>
 					<br></br>
 
-					 <a href="mathematic.php"><h1 style="color:White"><big><b><i><center>Прикладная математика</center></i></b></big></h1></a>
+					 <a href="mathematic.php"><h1 style="color:White"><big><center>Прикладная математика</center></big></h1></a>
 					<br></br>
-					 <a href="system.php"> <h1 style="color:White"> <big><b><i><center>Системное проектирование</center></i></b></big></h1></a>
+					 <a href="system.php"> <h1 style="color:White"> <big><center>Системное проектирование</center></big></h1></a>
 				</div>
 			</div>
 		</div>		
