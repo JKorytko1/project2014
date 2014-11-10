a653bfde13dad82e27f1688c136adbef10a613ae
JKorytko1 <julia_korytko@list.ru>
2014-11-10 15:43:57 +0200

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/project20141/';
+$config['base_url']	= 'http://classes.aasiutin.com/';
 
 /*
 |--------------------------------------------------------------------------
