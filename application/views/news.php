1521e0e79a662220bffd06f0cf0e77b035ce4955
JKorytko1 <julia_korytko@list.ru>
2014-11-18 22:53:03 +0200

    Update news.php

--- application/views/news.php
+++ application/views/news.php
@@ -19,7 +19,7 @@
         <div class="blog-post" >  
           <h2 class="blog-post-title">День открытых дверей в Политехе</h2>
           <h4 class="blog-post-meta">Ноябрь 18, 2014 by Admin</a></h4>
-          <h4 align="justify">
+          <h4 class="blog-post" align="justify">
            <img src="<?=base_url();?>assets/images/day1.jpg" style="padding: 5px 5px 0px 0px; float:left;" width="380" height="250" /><p>День открытых дверей в Национальном техническом университете «Харьковский политехнический институт» пройдет 30 ноября.
                 Как сообщили в университете, в этот день абитуриенты смогут пообщаться с руководством НТУ «ХПИ» - ректором, 
                 проректором, а также деканами факультетов, заведующими и представителями выпускающих кафедр и сотрудниками приемной 
@@ -33,7 +33,7 @@
         <div class="blog-post">
            <h2 class="blog-post-title">Изменения графика учебного процесса</h2>
            <h4 class="blog-post-meta">Октябрь 21, 2014 by Admin</a></h4>
-           <h4 align="justify">
+           <h4 class="blog-post" align="justify">
               <img src="<?=base_url();?>assets/images/day2.jpg" style="padding: 5px 5px 0px 0px; float:left;" width="380" height="250" />
                <p> В 2014–2015 учебный год внесены некоторые изменения отдельных элементов графика учебного процесса. 
                Однако эти изменения не коснутся количества отведенного для учебы времени и не повлияют на качество учебного процесса. 
@@ -52,7 +52,7 @@
            <h4 class="blog-post-meta">Октябрь 21, 2014 by Admin</a></h4>
           
           <img src="<?=base_url();?>assets/images/02.jpg" style="padding: 5px 5px 0px 0px; float:left;" width="300" height="200" />
-           <h4 align="justify">  
+           <h4 class="blog-post" align="justify">  
            Профессор Товажнянский Леонид Леонидович продолжает свою работу в должности ректора Национального технического университета 
            «Харьковский политехнический институт». После окончания очередного срока (контракта) 19 января 2015 года будет объявлен 
            конкурс на замещение должности ректора Национального технического университета «Харьковский политехнический институт» в
@@ -62,7 +62,7 @@
         <div class="blog-post">
            <h2 class="blog-post-title">ХХІІ Международная научно-практическая конференция MicroCAD - 2014</h2>
            <h4 class="blog-post-meta">Октябрь 21, 2014 by Admin</a></h4>
-            <h4 align="justify">
+            <h4 class="blog-post" align="justify">
               <img src="<?=base_url();?>assets/images/mat1.jpg" style="padding: 5px 5px 0px 0px; float:left;" width="300" height="200" />
             15-17 октября в Национальном техническом университете «Харьковский политехнический институт» 
             состоялась ХХІІ Международная научно-практическая конференция MicroCAD - 2014. Конференция проводилась 
@@ -74,7 +74,7 @@
         <div class="blog-post">
            <h2 class="blog-post-title">Группа СПУ "В Контакте"</h2>
             <h4 class="blog-post-meta">Октябрь 21, 2014 by Admin</h4>
-            <h4 align="justify">
+            <h4 class="blog-post" align="justify">
              <img src="<?=base_url();?>assets/images/spu.jpg" style="padding: 5px 5px 0px 0px; float:left;" width="250" height="200" />
             В социальной сети "В контакте" существует активно развивающаяся группа. В ней Вы можете найти интересную информацию и пообщаться с преподавателями кафедры.</h4>
            <h4>Присоединяйтесь! Адрес: <a href="http://vk.com/official_spu" target="_blank">http://vk.com/official_spu</a>
