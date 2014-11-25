01449e5a7fea1064fc84eb82c92792b61af25d15
ALysan <angelina_lysan@mail.ru>
2014-11-25 18:48:41 +0200

    speciality

--- application/views/speciality.php
+++ application/views/speciality.php
@@ -4,9 +4,10 @@
 				<div class="image-block">
 					<img src="<?=base_url();?>assets/images/comp.jpg" width="1000" height="800">
 				</div>
+				
 				<div class="text-block" style="margin-top: -800px;">
-					<center>
-						</br><br>   </br><br>
+						<center>
+						</br><br></br><br>
 						<p style="color:#FFE4E1;"><big><b>СПЕЦИАЛЬНОСТИ КАФЕДРЫ</b></big></p>
 						<br></br></br><br>   </br><br> 
 						<a href="inform.php"><p style="color:White;"><big><center>Информатика</center></big></p></a>
@@ -14,9 +15,10 @@
 						<a href="mathematic.php"><p style="color:White"><big><center>Прикладная математика</center></big></p></a>
 						<br></br> </br><br> 
 						<a href="system.php"> <p style="color:White"> <big><center>Системное проектирование</center></big></p></a>
-					</center>
+						</center>
 				</div>
 				<div class="other" style="margin-top: 350px;"></div>
 			</div>
-		</div>		
-	</div>
+		</div>
+</div>		
+	
