ad298ecc770042c49f079c93f409bacc3a14619a
ALysan <angelina_lysan@mail.ru>
2014-11-26 13:39:23 +0200

    speliality

--- application/views/speciality.php
+++ application/views/speciality.php
@@ -4,21 +4,19 @@
 				<div class="image-block">
 					<img src="<?=base_url();?>assets/images/comp.jpg" width="1000" height="800">
 				</div>
-				
 				<div class="text-block" style="margin-top: -800px;">
-						<center>
-						</br><br></br><br>
-						<p style="color:#FFE4E1;"><big><b>СПЕЦИАЛЬНОСТИ КАФЕДРЫ</b></big></p>
+					<center>
+						</br><br>   </br><br>
+						<h1 style="color:#FFE4E1;"><big><b>СПЕЦИАЛЬНОСТИ КАФЕДРЫ</b></big></h1>
 						<br></br></br><br>   </br><br> 
-						<a href="inform.php"><p style="color:White;"><big><center>Информатика</center></big></p></a>
-						<br></br> </br><br> 
-						<a href="mathematic.php"><p style="color:White"><big><center>Прикладная математика</center></big></p></a>
-						<br></br> </br><br> 
-						<a href="system.php"> <p style="color:White"> <big><center>Системное проектирование</center></big></p></a>
-						</center>
+						<a href="inform.php"><h1 style="color:White;"><big><center>Информатика</center></big></h1></a>
+						<br></br>
+						<a href="mathematic.php"><h1 style="color:White"><big><center>Прикладная математика</center></big></h1></a>
+						<br></br>
+						<a href="system.php"> <h1 style="color:White"> <big><center>Системное проектирование</center></big></h1></a>
+					</center>
 				</div>
 				<div class="other" style="margin-top: 350px;"></div>
 			</div>
-		</div>
-</div>		
-	
+		</div>		
+	</div>
