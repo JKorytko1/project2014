2aa44ea03d666d22129232c30fad3f761a6a5639
OKyrylchenko <Kyrylchenko@list.ru>
2014-12-11 23:42:53 +0200

    articles

--- application/views/articles.php
+++ application/views/articles.php
@@ -11,138 +11,138 @@
 
  
  
- <p><big><a href="<?=base_url();?>assets/articles/Breslavsky_Breslavskaya_Tatarinova.pdf" target="blank">ВПЛИВ КОСМІЧНОЇ РАДІАЦІЇ ТА ЦИКЛІЧНОСТІ
- НАВАНТАЖЕННЯ <br>НА ПОШКОДЖУВАНІСТЬ СТРИЖНЕВИХ ЕЛЕМЕНТІВ</a></big></p>
-<p> <big><i><b>БРЕСЛАВСЬКИЙ Д.В., д-р техн. наук, БРЕСЛАВСЬКА О.О., канд. техн. 
-наук,<p> ТАТАРІНОВА О.А., канд. техн. наук, НТУ «ХПІ»</p>
+<h3><a href="<?=base_url();?>assets/articles/Breslavsky_Breslavskaya_Tatarinova.pdf" target="blank">Вплив космічної радіації та циклічності 
+навантаження на пошкоджуваність стрижневих елементів</a></h3> 
+<p> <big><i><b>Бреславський Д.В., д-р техн. наук, Бреславська О.О., канд. техн. 
+наук,<p> Татарінова О.А., канд. техн. наук, НТУ «ХПІ»</p>
 </b></i></big></p>
  </br>
- <big><p>В статті надано рівняння стану теплової та радіаційної повзучості, що супроводжується </p>
-<p> механізмами незворотної пошкоджуваності, які сформульовані для випадку періодичної зміни </p>
- <p>температур та напружень. Рівняння запропоновано для аналізу деформування та довготривалої </p> 
- <p>міцності конструктивних елементів, що експлуатуються на космічних літальних апаратах. </p>
- <p>Розглянуто вплив механізмів пошкоджуваності внаслідок теплової повзучості, електронного </p>
- <p>опромінювання й циклічності змінювання температур на швидкість накопичування пошкоджень  </p>
- <p>й час до закінчення прихованого руйнування полімерного стрижня.</p></big>
+ <big><p>В статті надано рівняння стану теплової та радіаційної повзучості, що супроводжується 
+ механізмами незворотної пошкоджуваності, які сформульовані для випадку періодичної зміни 
+ температур та напружень. Рівняння запропоновано для аналізу деформування та довготривалої 
+ міцності конструктивних елементів, що експлуатуються на космічних літальних апаратах.
+ Розглянуто вплив механізмів пошкоджуваності внаслідок теплової повзучості, електронного 
+ опромінювання й циклічності змінювання температур на швидкість накопичування пошкоджень  
+ й час до закінчення прихованого руйнування полімерного стрижня.</p></big>
  </br>
  
- <p><big><a href="<?=base_url();?>assets/articles/Breslavsky_Tatarinova_Korytko.pdf" target="blank">ПОЛЗУЧЕСТЬ ОБОЛОЧЕК ВРАЩЕНИЯ В УСЛОВИЯХ СОВМЕСТНОГО 
- <br>ДЕЙСТВИЯ ПЕРИОДИЧЕСКИ ИЗМЕНЯЮЩИХСЯ 
-  <br>ТЕМПЕРАТУР И НАПРЯЖЕНИЙ</a></big></p>
-  <p><big><i><b>БРЕСЛАВСКИЙ Д.В., д-р техн. наук, ТАТАРИНОВА О.А., канд. техн. наук, 
-<p>КОРЫТКО Ю.Н., канд. техн. наук, НТУ «ХПИ»</p></b></i></big></p>
+ <h3><a href="<?=base_url();?>assets/articles/Breslavsky_Tatarinova_Korytko.pdf" target="blank">Ползучесть оболочек врещения в условиях совместного 
+ действия периодически изменяющихся температур и напряжений</a></h3>
+  <p><big><i><b>Бреславский Д.В., д-р техн. наук, Татаринова О.А., канд. техн. наук, 
+<p>Корытко Ю.Н., канд. техн. наук, НТУ «ХПИ»</p></b></i></big></p>
  </br>
- <big><p>Статтю присвячено опису постановки задачі та методу розв’язку задач повзучості </p>
-<p>оболонок обертання в умовах спільної дії температур та напружень, що періодично змінюються </p>
- <p>за часом. Для цього випадку надано рівняння стану повзучості та пошкоджуваності матеріалу.  </p> 
- <p>Наведено приклад чисельного розв’язання задачі повзучості циліндричної оболонки.  </p>
+ <big><p>Статтю присвячено опису постановки задачі та методу розв’язку задач повзучості 
+оболонок обертання в умовах спільної дії температур та напружень, що періодично змінюються 
+ за часом. Для цього випадку надано рівняння стану повзучості та пошкоджуваності матеріалу.   
+ Наведено приклад чисельного розв’язання задачі повзучості циліндричної оболонки.  </p>
  </big>
  </br>
  
- <p><big><a href="<?=base_url();?>assets/articles/LYAPUNOV_Creep.pdf" target="blank">ВОЗДЕЙСТВИЕ БЫСТРО ОСЦИЛЛИРУЮЩЕГО ПОЛЯ НА  
- <br> ДЕФОРМИРУЕМЫЕ ТЕЛА ПРИ НЕЛИНЕЙНОЙ ПОЛЗУЧЕСТИ</a></big></p>
- <p><big><i><b>БРЕСЛАВСЬКИЙ Д.В., д-р техн. наук, МОРАЧКОВСКИЙ О.К.</b></i></big></p>
+ <h3><a href="<?=base_url();?>assets/articles/LYAPUNOV_Creep.pdf" target="blank">Воздействие быстро осциллирующего поля на 
+ деформируемые тела при нелинейной ползучести</a></h3>
+ <p><big><i><b>Бреславский Д.В., д-р техн. наук, Морачковский О.К.</b></i></big></p>
   </br>
- <big><p>Запропоновано постановку і метод розв'язання задач нелінійної повзучості тіл,</p>
-<p> навантажених квазістатично змінними об'ємними і поверхневими силами та</p>
- <p>Додатковими поверхневими навантаженнями, швидко осцилюючими в часі.</p> 
- <p>Розв'язки знайдено на основі асимптотичних розкладів методу багатьох масштабів.</p>
- <p>Вивчено вплив на повзучість тіл гармонічної зовнішньої сили, алгоритм числових</p>
- <p>розрахунків за схемою методу скінченних елементів в поєднанні з кроковими методами </p>
- <p>інтегрування початково-крайових задач для «швидких» </p>
- <p>і «повільних» функцій асимптотичного зображення розв'язків.</big></p>
+ <big><p>Запропоновано постановку і метод розв'язання задач нелінійної повзучості тіл,
+ навантажених квазістатично змінними об'ємними і поверхневими силами та
+ Додатковими поверхневими навантаженнями, швидко осцилюючими в часі.
+ Розв'язки знайдено на основі асимптотичних розкладів методу багатьох масштабів.
+ Вивчено вплив на повзучість тіл гармонічної зовнішньої сили, алгоритм числових
+ розрахунків за схемою методу скінченних елементів в поєднанні з кроковими методами 
+ інтегрування початково-крайових задач для «швидких» 
+ і «повільних» функцій асимптотичного зображення розв'язків.</big></p>
  </br>
  
- <p><big><a href="<?=base_url();?>assets/articles/Breslavsky Tatarinova Morachkovsky.pdf" target="blank">HIGH-TEMPERATURE CREEP AND LONG-TERM STRENGTH
-<br>OF STRUCTURAL ELEMENTS UNDER CYCLIC LOADING</a></big></p>
-<p><big><i><b>D. V. BRESLAVSKY, O. K. MORACHKOVSKY,
-and O. A. TATARINOVA</b></i></big></p>
+ <h3><a href="<?=base_url();?>assets/articles/Breslavsky Tatarinova Morachkovsky.pdf" target="blank">High-temperature creep and long-term strength 
+ of structural elements under cyclic loading</a></h3>
+<p><big><i><b>D. V. Breslavsky, O. K. Morachkovsky,
+and O. A. Tatarinova</b></i></big></p>
  </br>
- <big><p>We present a method for solving problems of high-temperature cyclic creep and damage </p>
-<p>accumulation in structural elements. The asymptotic expansion and averaging techniques both over</p>
- <p>the period of forced vibrations of a body and that of slowly varying loads are used for the set of </p> 
- <p>equations describing the creep and damage processes in thin-walled structural elements.  </p>
+ <big><p>We present a method for solving problems of high-temperature cyclic creep and damage 
+accumulation in structural elements. The asymptotic expansion and averaging techniques both over
+ the period of forced vibrations of a body and that of slowly varying loads are used for the set of  
+ equations describing the creep and damage processes in thin-walled structural elements.  </p>
  </big>
  </br>
  
-  <p><big><a href="<?=base_url();?>assets/articles/Breslavsky_Korytko.pdf" target="blank">РОЗРАХУНКОВЕ ОЦІНЮВАННЯ ДОВГОТРИВАЛОЇ 
-  МІЦНОСТІ ЛОПАТОК <br>ГАЗОТУРБИННИХ ДВИГУНІВ ПРИ ЦИКЛІЧНИХ ТЕПЛОЗМІНАХ</a></big></p>
- <p><big><i><b>БРЕСЛАВСЬКИЙ Д.В., д-р техн. наук, КОРЫТКО Ю.Н., канд. техн. наук, НТУ «ХПИ»
+
+ 
+  <h3><a href="<?=base_url();?>assets/articles/Breslavsky_Korytko_Morachkovsky.pdf" target="blank">Модель циклічної термоповзучості для тіл обертання</a></h3>
+ <p><big><i><b>Бреславский Д.В., д-р техн. наук, Корытко Ю.Н., канд. техн. наук, НТУ «ХПИ»,<p>Морачковский О.К.</p>
 </b></i></big></p>
  </br>
- <big><p>В статье описаны постановка задачи, метод решения и результаты расчетов ползучести и </p>
-<p> длительной прочности лопаток газотурбинных двигателей, находящихся под действием </p>
- <p>циклически изменяющихся термосиловых нагрузок. Для верификации метода использованы </p> 
- <p>результаты экспериментального изучения моделей лопаток. Приводятся результаты расчетов. </p></big>
+ <big><p>Запропоновано новий метод розв'язку задач термоповзучості з урахуванням циклічної зміни
+зовнішніх силових і теплових полів. За допомогою методу асимптотичних розкладань й
+ усерднення на періоді  отримано основну і допоміжну системи рівнянь, запропоновано та 
+ перевірено рівняння стану циклічної термоповзучості. Проведено розрахункові дослідження 
+ повзучості циліндрів за циклічної зміни температури. </p></big>
  </br>
  
-  <p><big><a href="<?=base_url();?>assets/articles/Breslavsky_Korytko_Morachkovsky.pdf" target="blank">МОДЕЛЬ ЦИКЛІЧНОЇ ТЕРМОПОВЗУЧОСТІ ДЛЯ ТІЛ ОБЕРТАННЯ</a></big></p>
- <p><big><i><b>БРЕСЛАВСЬКИЙ Д.В., д-р техн. наук, КОРЫТКО Ю.Н., канд. техн. наук, НТУ «ХПИ»,<p>МОРАЧКОВСКИЙ О.К.</p>
+   <h3><a href="<?=base_url();?>assets/articles/Breslavsky_Korytko.pdf" target="blank">Розрахункове оцінювання довготривалої міцності
+  лопаток газотурбинних двигунів про циклічних теплозмінах</a></h3>
+ <p><big><i><b>Бреславский Д.В., д-р техн. наук, Корытко Ю.Н., канд. техн. наук, НТУ «ХПИ»
 </b></i></big></p>
  </br>
- <big><p>Запропоновано новий метод розв'язку задач термоповзучості з урахуванням циклічної зміни</p>
-<p> зовнішніх силових і теплових полів. За допомогою методу асимптотичних розкладань й</p>
- <p>усерднення на періоді  отримано основну і допоміжну системи рівнянь, запропоновано та</p> 
- <p>перевірено рівняння стану циклічної термоповзучості. Проведено розрахункові дослідження </p>
- <p>повзучості циліндрів за циклічної зміни температури. </p></big>
+ 
+ <big><p>В статье описаны постановка задачи, метод решения и результаты расчетов ползучести и 
+ длительной прочности лопаток газотурбинных двигателей, находящихся под действием 
+ циклически изменяющихся термосиловых нагрузок. Для верификации метода использованы  
+ результаты экспериментального изучения моделей лопаток. Приводятся результаты расчетов. </p></big>
  </br>
  
-  <p><big><a href="<?=base_url();?>assets/articles/Breslavsky_Numerical_Calculations.pdf" target="blank">NUMERICAL CALCULATIONS OF CREEP DAMAGE AT
-<p>CYCLIC LOADING BY USE OF TENSOR DAMAGE</p>
-PARAMETER MODEL</a></big></p>
- <p><big><i><b>D. V. BRESLAVSKY, O. K. MORACHKOVSKY, YU. KORYTKO,<p> V. MIETIELOV, O. TATARINOVA</p>
+  <h3><a href="<?=base_url();?>assets/articles/Breslavsky_Numerical_Calculations.pdf" target="blank">Numerical calculations of creep damage at cyclic 
+  loading by use of tensor damage parameter model</a></h3>
+ <p><big><i><b>D. V. Breslavsky, O. K. Morachkovsky, YU. Korytko,<p> V. Mietielov, O. Tatarinova</p>
 </b></i></big></p>
  </br>
- <big><p>Creep  of  cyclically  loaded  bodies  and  their  fracture  due  to  creep  mechanisms  are  studied.  Creep </p>
-<p>damage equations are built by use of tensor parameter for titanium alloy.  Similar equations were </p>
- <p>derived  for  stress  cyclic  varying using the method of asymptotic  expansions  and averaging in a </p> 
- <p>period.  2d  plane  stress  problems  were  solved  by  FEM  and  fields  of  stress,  strain,  displacement, </p>
- <p>damage parameter components as well as time to rupture values were obtained. </p></big>
+ <big><p>Creep  of  cyclically  loaded  bodies  and  their  fracture  due  to  creep  mechanisms  are  studied.  Creep 
+damage equations are built by use of tensor parameter for titanium alloy.  Similar equations were 
+ derived  for  stress  cyclic  varying using the method of asymptotic  expansions  and averaging in a  
+ period.  2d  plane  stress  problems  were  solved  by  FEM  and  fields  of  stress,  strain,  displacement, 
+ damage parameter components as well as time to rupture values were obtained. </p></big>
  </br>
  
- <p><big><a href="<?=base_url();?>assets/articles/HIGH TEMPERATURE CREEP AND DAMAGE ACCUMULATION IN.pdf" target="blank">HIGH TEMPERATURE CREEP AND DAMAGE ACCUMULATION IN
-<p>CYCLICALLY LOADED AXISYMMETRICAL BODIES OF REVOLUTION</p></a></big></p>
- <p><big><i><b>D. V. BRESLAVSKY, O. K. MORACHKOVSKY, YU. KORYTKO</p>
+ <h3><a href="<?=base_url();?>assets/articles/HIGH TEMPERATURE CREEP AND DAMAGE ACCUMULATION IN.pdf" target="blank">High temperature creep and damage accumulation 
+ in cyclically loaded axisymmetrical bodies of revolution</p></a></h3>
+ <p><big><i><b>D. V. Breslavsky, O. K. Morachkovsky, YU. Korytko</p>
 </b></i></big></p>
  </br>
- <big><p>The paper presents the constitutive equations as well as the data of numerical</p>
-<p>simulation of creep-damage problems of cyclically loaded and heated axisymmetrical structural members. </p>
- <p>The procedure of constitutive equations deriving is discussed. The experimental and </p> 
- <p>numerical data have been obtained for cyclically heated specimens made from highquality </p>
- <p>steel were compared in order to verify the flow rule and damage parameter equation. The problem of   </p>
- <p>creep and damage accumulation in the nipples of the regenerator for catalytic cracking </p>
- <p>of petroleum was analyzed with consideration of different temperature cycle parameters. </p></big>
+ <big><p>The paper presents the constitutive equations as well as the data of numerical
+simulation of creep-damage problems of cyclically loaded and heated axisymmetrical structural members. 
+ The procedure of constitutive equations deriving is discussed. The experimental and  
+ numerical data have been obtained for cyclically heated specimens made from highquality 
+ steel were compared in order to verify the flow rule and damage parameter equation. The problem of   
+ creep and damage accumulation in the nipples of the regenerator for catalytic cracking 
+ of petroleum was analyzed with consideration of different temperature cycle parameters. </p></big>
  </br>
  
-  <p><big><a href="<?=base_url();?>assets/articles/Korytko.pdf" target="blank">ИССЛЕДОВАНИЯ ПОЛЗУЧЕСТИ И ДЛИТЕЛЬНОЙ ПРОЧНОСТИ ТРЕХКАНАЛЬНОЙ
-<p>ОХЛАЖДАЕМОЙ ЛОПАТКИ ГАЗОТУРБИННОГО ДВИГАТЕЛЯ</p></a></big></p>
- <p><big><i><b>КОРЫТКО Ю.Н., канд. техн. наук, НТУ «ХПИ»</p>
+  <h3><a href="<?=base_url();?>assets/articles/Korytko.pdf" target="blank">Исследования ползучести и длительной прочности 
+  трехканальной охлаждаемой лопатки газотурбинного двигателя</p></a></h3>
+ <p><big><i><b>Корытко Ю.Н., канд. техн. наук, НТУ «ХПИ»</p>
 </b></i></big></p>
  </br>
- <big><p>Представлено метод дослідження повзучості та довготривалої міцності конструктивних елементів, які фун-</p>
-<p>кціонують в умовах високих періодично змінюваних температур. Метод реалізований у вигляді програмного </p>
- <p>комплексу, в якому методом скінченних елементів виконується рішення двомірних задач теорії повзучості. За </p> 
- <p>використанням вказаного програмного забезпечення проведені комплексні розрахункові дослідження довговіч-</p>
- <p>ності охолоджуваних лопаток газотурбінного двигуна. Визначено вплив тиску газового потоку на зовнішній </p>
- <p>окрайці лопатки на час до руйнування.</p></big>
+ <big><p>Представлено метод дослідження повзучості та довготривалої міцності конструктивних елементів, які фун-
+кціонують в умовах високих періодично змінюваних температур. Метод реалізований у вигляді програмного 
+ комплексу, в якому методом скінченних елементів виконується рішення двомірних задач теорії повзучості. За  
+ використанням вказаного програмного забезпечення проведені комплексні розрахункові дослідження довговіч-
+ ності охолоджуваних лопаток газотурбінного двигуна. Визначено вплив тиску газового потоку на зовнішній 
+ окрайці лопатки на час до руйнування.</p></big>
  </br>
  
-   <p><big><a href="<?=base_url();?>assets/articles/Korytko_.pdf" target="blank">РОЗРОБКА МЕТОДУ РОЗРАХУНКУ ДОВГОВІЧНОСТІ ЕЛЕМЕНТІВ КОНСТРУКЦІЙ
-<p>ПРИ ПЕРІОДИЧНІЙ ЗМІНІ ТЕМПЕРАТУР І НАВАНТАЖЕНЬ</p></a></big></p>
- <p><big><i><b>КОРЫТКО Ю.Н., канд. техн. наук, НТУ «ХПИ»</p>
+   <h3><a href="<?=base_url();?>assets/articles/Korytko_.pdf" target="blank">Розробка методу розрахунку довговічності 
+   елементів конструкцій при періодичній зміні температур і навантажень</p></a></h3>
+ <p><big><i><b>Корытко Ю.Н., канд. техн. наук, НТУ «ХПИ»</p>
 </b></i></big>
  </br>
- <big><p>Всі основні теоретичні розробки дисертаційної роботи виконано за використанням методів</p>
-<p>теорії повзучості та континуальної механіки пошкоджуваності; для створення визначальних співвідношень, що </p>
- <p>враховують вплив періодичної дії температурно-силових полів, використані </p> 
- <p>асимптотичні методи; для розв’язання задач повзучості застосовано метод</p>
- <p>скінченних елементів разом з багатокроковим методом прогнозу-корекції </p>
- <p>інтегрування за часом; для розв’язання систем лінійних алгебраїчних рівнянь –метод Холецького.</p></big>
+ <big><p>Всі основні теоретичні розробки дисертаційної роботи виконано за використанням методів
+теорії повзучості та континуальної механіки пошкоджуваності; для створення визначальних співвідношень, що 
+ враховують вплив періодичної дії температурно-силових полів, використані  
+ асимптотичні методи; для розв’язання задач повзучості застосовано метод
+ скінченних елементів разом з багатокроковим методом прогнозу-корекції 
+ інтегрування за часом; для розв’язання систем лінійних алгебраїчних рівнянь –метод Холецького.</p></big>
  </br>
 <div class="blog-post" style="margin-top: 35px;">
 </div>
 </div>
 	</div>
 </div>
-
 <!-- <br> </br> -->
