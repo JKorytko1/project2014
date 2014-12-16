0e7619d08975d1f2cee4c8f352f16b3b39256143
JKorytko1 <julia_korytko@list.ru>
2014-12-16 22:18:26 +0200

    Update articles.php

--- application/views/articles.php
+++ application/views/articles.php
@@ -52,21 +52,35 @@
  і «повільних» функцій асимптотичного зображення розв'язків.</big></p>
  </br>
  
- 
-
- 
-  <h3><a href="<?=base_url();?>assets/articles/Breslavsky_Korytko_Morachkovsky.pdf" target="blank">Модель циклічної термоповзучості для тіл обертання</a></h3>
- <p><big><i><b>Бреславский Д.В., д-р техн. наук, Корытко Ю.Н., канд. техн. наук, НТУ «ХПИ»,<p>Морачковский О.К.</p>
-</b></i></big></p>
+  <h3><a href="<?=base_url();?>assets/articles/Breslavsky_Numerical_Calculations.pdf" target="blank">Numerical calculations of creep damage at cyclic
+ loading by use of tensor damage parameter model</a></h3>
+ <p><big><i><b>D. V. Breslavsky, O. K. Morachkovsky, YU. Korytko,<p> V. Mietielov, O. Tatarinova</p>
+ </b></i></big></p>
  </br>
- <big><p>Запропоновано новий метод розв'язку задач термоповзучості з урахуванням циклічної зміни
-зовнішніх силових і теплових полів. За допомогою методу асимптотичних розкладань й
- усерднення на періоді  отримано основну і допоміжну системи рівнянь, запропоновано та 
- перевірено рівняння стану циклічної термоповзучості. Проведено розрахункові дослідження 
- повзучості циліндрів за циклічної зміни температури. </p></big>
+ <big><p>Creep of cyclically loaded bodies and their fracture due to creep mechanisms are studied. Creep
+ damage equations are built by use of tensor parameter for titanium alloy. Similar equations were
+ derived for stress cyclic varying using the method of asymptotic expansions and averaging in a
+ period. 2d plane stress problems were solved by FEM and fields of stress, strain, displacement,
+ damage parameter components as well as time to rupture values were obtained. </p></big>
  </br>
  
-   <h3><a href="<?=base_url();?>assets/articles/Breslavsky_Korytko.pdf" target="blank">Розрахункове оцінювання довготривалої міцності
+ 
+  <h3><a href="<?=base_url();?>assets/articles/HIGH TEMPERATURE CREEP AND DAMAGE ACCUMULATION IN.pdf" target="blank">High temperature creep and damage accumulation
+ in cyclically loaded axisymmetrical bodies of revolution</p></a></h3>
+ <p><big><i><b>D. V. Breslavsky, O. K. Morachkovsky, YU. Korytko</p>
+ </b></i></big></p>
+ </br>
+ <big><p>The paper presents the constitutive equations as well as the data of numerical
+ simulation of creep-damage problems of cyclically loaded and heated axisymmetrical structural members.
+ The procedure of constitutive equations deriving is discussed. The experimental and
+ numerical data have been obtained for cyclically heated specimens made from highquality
+ steel were compared in order to verify the flow rule and damage parameter equation. The problem of
+ creep and damage accumulation in the nipples of the regenerator for catalytic cracking
+ of petroleum was analyzed with consideration of different temperature cycle parameters. </p></big>
+ </br>
+ 
+ 
+ <h3><a href="<?=base_url();?>assets/articles/Breslavsky_Korytko.pdf" target="blank">Розрахункове оцінювання довготривалої міцності
   лопаток газотурбинних двигунів про циклічних теплозмінах</a></h3>
  <p><big><i><b>Бреславский Д.В., д-р техн. наук, Корытко Ю.Н., канд. техн. наук, НТУ «ХПИ»
 </b></i></big></p>
@@ -78,9 +92,6 @@
  результаты экспериментального изучения моделей лопаток. Приводятся результаты расчетов. </p></big>
  </br>
  
- 
- 
-
  
   <h3><a href="<?=base_url();?>assets/articles/Korytko.pdf" target="blank">Исследования ползучести и длительной прочности 
   трехканальной охлаждаемой лопатки газотурбинного двигателя</p></a></h3>
@@ -95,21 +106,8 @@
  окрайці лопатки на час до руйнування.</p></big>
  </br>
  
-   <h3><a href="<?=base_url();?>assets/articles/Korytko_.pdf" target="blank">Розробка методу розрахунку довговічності 
-   елементів конструкцій при періодичній зміні температур і навантажень</p></a></h3>
- <p><big><i><b>Корытко Ю.Н., канд. техн. наук, НТУ «ХПИ»</p>
-</b></i></big>
- </br>
- <big><p>Всі основні теоретичні розробки дисертаційної роботи виконано за використанням методів
-теорії повзучості та континуальної механіки пошкоджуваності; для створення визначальних співвідношень, що 
- враховують вплив періодичної дії температурно-силових полів, використані  
- асимптотичні методи; для розв’язання задач повзучості застосовано метод
- скінченних елементів разом з багатокроковим методом прогнозу-корекції 
- інтегрування за часом; для розв’язання систем лінійних алгебраїчних рівнянь –метод Холецького.</p></big>
- </br>
-<div class="blog-post" style="margin-top: 35px;">
+   
 </div>
 </div>
-	</div>
 </div>
 <!-- <br> </br> -->
